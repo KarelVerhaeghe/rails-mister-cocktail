0847ffaa44b7271fde4eae2d856f464f60502012
KarelVerhaeghe <karelverhaeghe@hotmail.com>
2017-11-02 20:07:35 +0100

    Cocktails, with all functions working

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,28 +1,37 @@
 class DosesController < ApplicationController
   def new
-    @cocktail = Cocktail.find(params[:cocktail_id])
+    set_cocktail
+    @ingredients = Ingredients.all - @cocktail.ingredients
     @dose = Dose.new
   end
 
   def create
+    @cocktail = set_cocktail
     @dose = Dose.new({description: dose_params[:description]})
-    @dose.ingredient = Ingredient.find(dose_params[:ingredient].to_i)
-    @dose.cocktail = Cocktail.find(params[:cocktail_id])
+    @dose.ingredient = Ingredient.find(dose_params[:ingredient_id])
+    @dose.cocktail = @cocktail
     if @dose.save!
-      redirect_to cocktail_path(@dose.cocktail)
+      redirect_to cocktail_path(@cocktail)
     else
+      @ingredients = Ingredients.all - @cocktail.ingredients
       render :new
     end
   end
 
   def destroy
     @dose = Dose.find(params[:id])
+    @cocktail = @dose.cocktail
     @dose.destroy
+    redirect_to cocktail_path(@cocktail)
   end
 
   private
 
   def dose_params
-    params.require(:dose).permit(:ingredient, :description)
+    params.require(:dose).permit(:ingredient_id, :description)
+  end
+
+  def set_cocktail #because this param comes from somewhere elese , the url instead of the form
+    @cocktail = Cocktail.find(params[:cocktail_id])
   end
 end
